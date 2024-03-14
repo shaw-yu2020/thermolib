8b22d17320b48c893958eb5b6949b7de2c9a7d4f
shaw-code <ygl1998@yeah.net>
2024-03-14 16:05:26 +0800

    优化 将空值枚举调整为错误枚举

--- src/helmholtz/helmholtz_pure.rs
+++ src/helmholtz/helmholtz_pure.rs
@@ -3,6 +3,7 @@ use super::ancillary_equations::SaturatedVaporDensityEquation;
 use super::ancillary_equations::VaporPressureEquation;
 use super::real_helmholtz_equation::RealHelmholtzEquation;
 use super::ThermoProp;
+use crate::pubtraits::MyErr;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Read;
@@ -19,18 +20,18 @@ pub struct HelmholtzPure {
     rhols: SaturatedLiquidDensityEquation,
     omega: f64, // 偏心因子
 }
-pub fn read_json(path: &str) -> Option<HelmholtzPure> {
+pub fn read_json(path: &str) -> Result<HelmholtzPure, MyErr> {
     let mut str_json = String::new();
     let _ = match File::open(&Path::new(path)) {
         Ok(file) => file,
         Err(_) => match File::open(&Path::new(env!("CARGO_MANIFEST_DIR")).join("res").join(path)) {
             Ok(file) => file,
-            Err(_) => return None,
+            Err(_) => return Err(MyErr::new(&format!("couldn't find {}", path))),
         },
     }
     .read_to_string(&mut str_json);
     match serde_json::from_str(&str_json) {
-        Ok(hp) => Some(hp),
-        Err(_) => None,
+        Ok(hp) => Ok(hp),
+        Err(_) => Err(MyErr::new(&format!("no alpha(HelmholtzPure) in {}", path))),
     }
 }
