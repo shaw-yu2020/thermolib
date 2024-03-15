b3a7f09f706f9137f665f510a906ba74062c734b
shaw-code <ygl1998@yeah.net>
2024-03-15 22:29:59 +0800

    添加 PR方程计算密度的代码

--- src/helmholtz/helmholtz_pure.rs
+++ src/helmholtz/helmholtz_pure.rs
@@ -203,6 +203,55 @@ impl Prop for HelmholtzPure {
         }
     }
 }
+#[allow(non_snake_case)]
+fn calc_density_from_pr(T: f64, P: f64, Tc: f64, Pc: f64, R: f64, omega: f64) -> f64 {
+    let kappa = 0.37464 + 1.54226 * omega - 0.26992 * omega.powi(2);
+    let a = 0.45724 * R.powi(2) * Tc.powi(2) / Pc * (1.0 + kappa * (1.0 - (T / Tc).sqrt())).powi(2);
+    let b = 0.07780 * R * Tc / Pc; // b=bc
+    let A = a * P / R.powi(2) / T.powi(2);
+    let B = b * P / R / T;
+    // 以下是计算一元三次方程的代码
+    let b = -(1.0 - B);
+    let c = A - 3.0 * B.powi(2) - 2.0 * B;
+    let d = -(A * B - B.powi(2) - B.powi(3));
+    let A = b.powi(2) - 3.0 * c;
+    let B = b * c - 9.0 * d;
+    let C = c.powi(2) - 3.0 * b * d;
+    let Delta = B.powi(2) - 4.0 * A * C;
+    let Zg: f64;
+    let mut Zl: f64 = 0.0;
+    if Delta > 0.0 {
+        // Delta>0 方程有一个正实根和一对共轭复根
+        // 该情况对应立方型方程的单相区 仅保留实根！!
+        let Y1 = A * b + 1.5 * (-B + Delta.sqrt());
+        let Y2 = A * b + 1.5 * (-B - Delta.sqrt());
+        Zg = (-b - (Y1.cbrt() + Y2.cbrt())) / 3.0;
+    } else {
+        // Delta<0 方程有三个不相等的实根
+        // 该情况对应立方型方程的两相区 舍弃中间根！！
+        let theta3 = ((2.0 * A * b - 3.0 * B) / (2.0 * A * A.sqrt())).acos() / 3.0;
+        let x1 = (-b - 2.0 * A.sqrt() * theta3.cos()) / 3.0;
+        let x2 = (-b + A.sqrt() * (theta3.cos() + f64::sqrt(3.0) * theta3.sin())) / 3.0;
+        let x3 = (-b + A.sqrt() * (theta3.cos() - f64::sqrt(3.0) * theta3.sin())) / 3.0;
+        Zg = x1.max(x2).max(x3);
+        Zl = x1.min(x2).min(x3);
+    }
+    if Zl <= 0.0 {
+        P / (Zg * R * T)
+    } else {
+        let lnfpg = (Zg - 1.0)
+            - (Zg - B).ln()
+            - A / (2.0 * f64::sqrt(2.0) * B) * ((Zg + 2.414 * B) / (Zg - 0.414 * B)).ln();
+        let lnfpl = (Zl - 1.0)
+            - (Zl - B).ln()
+            - A / (2.0 * f64::sqrt(2.0) * B) * ((Zl + 2.414 * B) / (Zl - 0.414 * B)).ln();
+        if lnfpg < lnfpl {
+            P / (Zg * R * T)
+        } else {
+            P / (Zl * R * T)
+        }
+    }
+}
 // 单元测试
 #[cfg(test)]
 mod tests {
