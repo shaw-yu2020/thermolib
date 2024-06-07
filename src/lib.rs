6d21248eee47003f527ff914811af6145cbda568
shaw-code <ygl1998@yeah.net>
2024-06-07 10:57:06 +0800

    初始化

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! 
+//! thermolib
+//! =========
+//! 
+//! An open-source library for 
+//! the calculation of fluid properties.
+//! 
+//! 
+/// Python wrappers
+mod python;
---
+++ src/python.rs
@@ -0,0 +1,13 @@
+/// A Python module implemented in Rust.
+use pyo3::prelude::*;
+#[pyfunction]
+fn hello() -> PyResult<String> {
+    Ok(String::from("Hello, Rust And Python."))
+}
+/// A Python module implemented in Rust.
+#[pymodule]
+#[pyo3(name = "thermolib")] // Rename pymodule
+pub fn pylib(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(hello, m)?)?;
+    Ok(())
+}
