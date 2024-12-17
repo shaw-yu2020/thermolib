833dbd2ae1ea642eae10f7641d913915f6bd36a9
shaw-code <ygl1998@yeah.net>
2024-12-17 12:06:11 +0800

    升级 封装代码的版本

--- src/python.rs
+++ src/python.rs
@@ -14,7 +14,7 @@ use crate::Vdw;
 /// A Python module implemented in Rust.
 #[pymodule]
 #[pyo3(name = "thermolib")] // Rename pymodule
-pub fn pylib(_py: Python, m: &PyModule) -> PyResult<()> {
+pub fn pylib(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hello, m)?)?;
     m.add_class::<Vdw>()?;
     m.add_class::<Rk>()?;
