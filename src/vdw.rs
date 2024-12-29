8cf4e0bd84ea9ede22519f29f5000d1ff55fa494
潇潇的TB\ygl <ygl1998@yeah.net>
2024-12-29 14:23:05 +0800

    优化 部分代码

--- src/vdw.rs
+++ src/vdw.rs
@@ -49,8 +49,8 @@ pub struct Vdw {
     Zl: f64,
     is_single_phase: bool,
 }
-#[allow(non_snake_case)]
 impl Vdw {
+    #[allow(non_snake_case)]
     pub fn new_fluid(Tc: f64, pc: f64, M: f64) -> Self {
         let R = 8.314462618;
         let Zc = 3.0 / 8.0;
