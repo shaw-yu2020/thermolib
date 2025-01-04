d4af6bbf245c1fa5bc17189c6b3f9e95a51b694e
潇潇的TB\ygl <ygl1998@yeah.net>
2025-01-04 23:52:15 +0800

    优化 VDW方程

--- src/vdw.rs
+++ src/vdw.rs
@@ -8,6 +8,7 @@ enum VdwErr {
     #[error("property not in single phase")]
     NotInSinglePhase,
 }
+use crate::algorithms::shengjin_roots;
 use anyhow::anyhow;
 #[cfg(feature = "with_pyo3")]
 use pyo3::{pyclass, pymethods};
@@ -80,33 +81,13 @@ impl Vdw {
     fn calc_root(&mut self) {
         self.A = self.a * self.p / (self.R * self.T).powi(2);
         self.B = self.b * self.p / (self.R * self.T);
-        let b = -(self.B + 1.0);
-        let c = self.A;
-        let d = -self.A * self.B;
-        let A = b.powi(2) - 3.0 * c;
-        let B = b * c - 9.0 * d;
-        let C = c.powi(2) - 3.0 * b * d;
-        let Delta = B.powi(2) - 4.0 * A * C;
-        if Delta.is_sign_negative() {
-            let theta3 = ((2.0 * A * b - 3.0 * B) / (2.0 * A * A.sqrt())).acos() / 3.0;
-            let x1 = (-b - 2.0 * A.sqrt() * theta3.cos()) / 3.0;
-            let x2 = (-b + A.sqrt() * (theta3.cos() + 3_f64.sqrt() * theta3.sin())) / 3.0;
-            let x3 = (-b + A.sqrt() * (theta3.cos() - 3_f64.sqrt() * theta3.sin())) / 3.0;
-            let Zv = x1.max(x2).max(x3);
-            let Zl = x1.min(x2).min(x3);
-            if Zl.is_sign_negative() {
-                self.Z = Zv;
-                self.is_single_phase = true;
-            } else {
-                self.Zv = Zv;
-                self.Zl = Zl;
-                self.is_single_phase = false;
-            }
-        } else {
-            let Y1 = A * b + 1.5 * (-B + Delta.sqrt());
-            let Y2 = A * b + 1.5 * (-B - Delta.sqrt());
-            self.Z = (-b - (Y1.cbrt() + Y2.cbrt())) / 3.0;
+        let (Zv, Zl) = shengjin_roots(-self.B - 1.0, self.A, -self.A * self.B);
+        if Zl == 0.0 {
+            self.Z = Zv;
             self.is_single_phase = true;
+        } else {
+            (self.Zv, self.Zl) = (Zv, Zl);
+            self.is_single_phase = false;
         }
     }
     fn calc_lnfp(&self, Z: f64) -> f64 {
