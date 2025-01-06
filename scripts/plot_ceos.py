59a2f57f60d21c3de35d3cc226b798bf88714b3c
shaw-code <ygl1998@yeah.net>
2025-01-06 20:10:58 +0800

    优化 绘图脚本

--- scripts/plot_ceos.py
+++ scripts/plot_ceos.py
@@ -1,40 +1,31 @@
-"""
-plot ceos
-"""
+"""plot ceos"""
 
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from thermolib import hello, Vdw, Rk, Srk, Pr  # pylint: disable=no-name-in-module
 
-# pylint: disable=E
-from thermolib import hello, Vdw, Rk, Srk, Pr
 
 print("test_hello:", hello())
+
 
 TC = 430.64
 PC = 7886600
 OMEGA = 0.256
-M = 0.064064
+vdw = Vdw(TC, PC)
+rk = Rk(TC, PC)
+srk = Srk(TC, PC, OMEGA)
+pr = Pr(TC, PC, OMEGA)
 
-vdw = Vdw(TC, PC, M)
-vdw.set_mass_unit()
-
-rk = Rk(TC, PC, M)
-rk.set_mass_unit()
-
-srk = Srk(TC, PC, OMEGA, M)
-srk.set_mass_unit()
-
-pr = Pr(TC, PC, OMEGA, M)
-pr.set_mass_unit()
 
 fig, (ax1, ax2) = plt.subplots(2, 1)
+
 
 t_s = []
 p_s = []
 rho_v = []
 rho_l = []
-for T in range(math.floor(0.7 * TC), math.ceil(TC)):
+for T in range(math.floor(0.6 * TC), math.ceil(TC)):
     vdw.t_flash(T)
     t_s.append(vdw.T_s())
     p_s.append(vdw.p_s())
@@ -44,16 +35,16 @@ t_s = np.array(t_s)
 p_s = np.array(p_s)
 rho_v = np.array(rho_v)
 rho_l = np.array(rho_l)
+ax1.plot(t_s, p_s, color="r", label="Vdw")
+ax2.plot(rho_v, p_s, color="r", label="Vdw")
+ax2.plot(rho_l, p_s, color="r")
 
-ax1.plot(t_s, p_s, color="red", label="Vdw")
-ax2.plot(rho_v, p_s, color="red", label="Vdw")
-ax2.plot(rho_l, p_s, color="red")
 
 t_s = []
 p_s = []
 rho_v = []
 rho_l = []
-for T in range(math.floor(0.7 * TC), math.ceil(TC)):
+for T in range(math.floor(0.6 * TC), math.ceil(TC)):
     rk.t_flash(T)
     t_s.append(rk.T_s())
     p_s.append(rk.p_s())
@@ -63,16 +54,16 @@ t_s = np.array(t_s)
 p_s = np.array(p_s)
 rho_v = np.array(rho_v)
 rho_l = np.array(rho_l)
+ax1.plot(t_s, p_s, color="g", label="Rk")
+ax2.plot(rho_v, p_s, color="g", label="Rk")
+ax2.plot(rho_l, p_s, color="g")
 
-ax1.plot(t_s, p_s, color="yellow", label="Rk")
-ax2.plot(rho_v, p_s, color="yellow", label="Rk")
-ax2.plot(rho_l, p_s, color="yellow")
 
 t_s = []
 p_s = []
 rho_v = []
 rho_l = []
-for T in range(math.floor(0.7 * TC), math.ceil(TC)):
+for T in range(math.floor(0.6 * TC), math.ceil(TC)):
     srk.t_flash(T)
     t_s.append(srk.T_s())
     p_s.append(srk.p_s())
@@ -82,16 +73,16 @@ t_s = np.array(t_s)
 p_s = np.array(p_s)
 rho_v = np.array(rho_v)
 rho_l = np.array(rho_l)
+ax1.plot(t_s, p_s, color="b", label="Srk")
+ax2.plot(rho_v, p_s, color="b", label="Srk")
+ax2.plot(rho_l, p_s, color="b")
 
-ax1.plot(t_s, p_s, color="blue", label="Srk")
-ax2.plot(rho_v, p_s, color="blue", label="Srk")
-ax2.plot(rho_l, p_s, color="blue")
 
 t_s = []
 p_s = []
 rho_v = []
 rho_l = []
-for T in range(math.floor(0.7 * TC), math.ceil(TC)):
+for T in range(math.floor(0.6 * TC), math.ceil(TC)):
     pr.t_flash(T)
     t_s.append(pr.T_s())
     p_s.append(pr.p_s())
@@ -101,19 +92,18 @@ t_s = np.array(t_s)
 p_s = np.array(p_s)
 rho_v = np.array(rho_v)
 rho_l = np.array(rho_l)
+ax1.plot(t_s, p_s, color="y", label="Pr")
+ax2.plot(rho_v, p_s, color="y", label="Pr")
+ax2.plot(rho_l, p_s, color="y")
 
-ax1.plot(t_s, p_s, color="green", label="Pr")
-ax2.plot(rho_v, p_s, color="green", label="Pr")
-ax2.plot(rho_l, p_s, color="green")
 
 ax1.set_xlabel("temperature K")
 ax1.set_ylabel("pressure Pa")
 ax1.legend()
-
-ax2.set_xlabel("density kg/m3")
+ax2.set_xlabel("density mol/m3")
 ax2.set_ylabel("pressure Pa")
 ax2.legend()
 
-plt.tight_layout()
 
+plt.tight_layout()
 plt.show()
